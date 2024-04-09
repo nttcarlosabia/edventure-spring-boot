78e8036e397453761afda595b592a666a8a298fe
carlos99at <carlos.abia99@gmail.com>
2024-04-09 22:28:54 +0200

    feat: event repo

---
+++ src/main/java/com/example/demo/controllers/EventController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Event;
+import com.example.demo.models.User;
+import com.example.demo.repositories.EventRepository;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @GetMapping
+    public List<Event> getEvents() {
+        return eventRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event request) {
+        try {
+
+            Event newEvent = new Event(request.getUserOwner(), request.getName(), request.getType(),
+                    request.getDescription(), request.getImage(),
+                    request.getDate(), request.getAddress(),
+                    request.getAssistantsExpected());
+            eventRepository.save(newEvent);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(newEvent);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEvent(@PathVariable Long id) {
+        eventRepository.deleteById(id);
+    }
+}
--- src/main/java/com/example/demo/models/Event.java
+++ src/main/java/com/example/demo/models/Event.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +14,11 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "events", schema = "public")
 public class Event {
@@ -22,14 +28,16 @@ public class Event {
     private Long id;
 
     @ManyToOne
+    @JsonIgnoreProperties(value = { "userEvents", "followingEvents" })
     private User userOwner;
 
     private String name;
 
     private String type;
 
-    @Lob
-    private byte[] image;
+    private String description;
+
+    private String image;
 
     private Date date;
 
@@ -43,81 +51,17 @@ public class Event {
     public Event() {
     }
 
-    public Event(Long id, User userOwner, String name, String type, byte[] image, Date date, String address,
+    public Event(User userOwner, String name, String type, String description, String image, Date date, String address,
             String assistantsExpected) {
-        this.id = id;
         this.userOwner = userOwner;
         this.name = name;
+        this.description = description;
         this.type = type;
         this.image = image;
         this.date = date;
         this.address = address;
         this.assistantsExpected = assistantsExpected;
         this.usersFollowing = new ArrayList<User>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public User getUserOwner() {
-        return userOwner;
-    }
-
-    public void setUserOwner(User name) {
-        this.userOwner = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(byte[] image) {
-        this.image = image;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getAssistantsExpected() {
-        return assistantsExpected;
-    }
-
-    public void setAssistantsExpected(String assistantsExpected) {
-        this.assistantsExpected = assistantsExpected;
-    }
-
-    public List<User> getusersFollowing() {
-        return usersFollowing;
-    }
-
-    public void setusersFollowing(List<User> usersFollowing) {
-        this.usersFollowing = usersFollowing;
     }
 
 }
