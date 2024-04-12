d1cba156dd737fd04f748d453b73f61924f29d12
carlos99at <carlos.abia99@gmail.com>
2024-04-12 12:02:10 +0200

    feat: get event by id

--- src/main/java/com/example/demo/controllers/EventController.java
+++ src/main/java/com/example/demo/controllers/EventController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,6 +44,20 @@ public class EventController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
         }
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getEventById(@PathVariable Long id) {
+        try {
+            Optional<Event> event = eventRepository.findById(id);
+            if (event.isPresent()) {
+                return ResponseEntity.status(HttpStatus.OK).body(event.get());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Event not found with id: " + id);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+        }
     }
 
     @DeleteMapping("/{id}")
